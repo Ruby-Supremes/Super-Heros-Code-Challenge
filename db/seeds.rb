a05831923e69f404de7406c0b761c1836c13cc3f
Wangari-Sharon <wangarik35@gmail.com>
2022-10-11 08:40:18 +0300

    Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "seeding data..."
+
+Hero.create(name: "Kamala Khan", super_name: "Ms. Marvel" )
+Hero.create(name: "Doreen Green", super_name: "Squirrel Girl")
+Hero.create(name: "Gwen Stacy", super_name: "Spider-Gwen")
+
+Power.create(name: "super strength",description: "gives the wielder super-human strengths")
+Power.create(name: "flight",description: "gives the wielder the ability to fly through the skies at supersonic speed")
+Power.create(name: "lightning speed",description: "gives the wielder super-sonic faster speed")
+
+HeroPower.create(strength: "Strong",power_id: 1,hero_id: 1)
+HeroPower.create(strength: "Weak",power_id: 3,hero_id: 2)
+HeroPower.create(strength: "Average",power_id: 2,hero_id: 3)
+
+puts "✅ Done seeding!"
